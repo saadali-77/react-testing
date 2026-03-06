94da2e4f4c6d935ffac3ede84d47f44cc951e339
saadali <saadali6991550@gmail.com>
2026-03-06 19:06:33 +0500

    11 filtering test

--- src/Components/Greet.test.tsx
+++ src/Components/Greet.test.tsx
@@ -1,11 +1,11 @@
 import { render,screen } from "@testing-library/react";
 import { Greet } from "./Greet";
-test('first greet test',()=>{
+test.skip('first greet test',()=>{
    render(<Greet/>)
  const linkelement=   screen.getByText(/hello/i)
  expect(linkelement).toBeInTheDocument()
 })
-test('first greet test 2',()=>{
+test.only('first greet test 2',()=>{
    render(<Greet name='saad'/>)
  const linkelement=   screen.getByText('Hello saad')
  expect(linkelement).toBeInTheDocument()
