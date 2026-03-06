c8a6fd93a5b0ed7feb3893fe3a436f5326303216
saadali <saadali6991550@gmail.com>
2026-03-06 19:22:27 +0500

    12 grouping test

--- src/Components/Greet.test.tsx
+++ src/Components/Greet.test.tsx
@@ -1,12 +1,24 @@
 import { render,screen } from "@testing-library/react";
 import { Greet } from "./Greet";
-test.skip('first greet test',()=>{
-   render(<Greet/>)
- const linkelement=   screen.getByText(/hello/i)
- expect(linkelement).toBeInTheDocument()
-})
-test.only('first greet test 2',()=>{
-   render(<Greet name='saad'/>)
- const linkelement=   screen.getByText('Hello saad')
- expect(linkelement).toBeInTheDocument()
+
+describe('Greets',()=>{
+
+    test('test 1',()=>{
+       render(<Greet/>)
+     const linkelement=   screen.getByText(/hello/i)
+     expect(linkelement).toBeInTheDocument()
+    })
+
+    describe('nested',()=>{
+
+        test(' test 2',()=>{
+           render(<Greet name='saad'/>)
+         const linkelement=   screen.getByText('Hello saad')
+         expect(linkelement).toBeInTheDocument()
+        })
+
+
+    })
+
+
 })
