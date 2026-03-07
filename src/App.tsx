67275cd46e3af275c3964118066416ae49749c65
saadali <saadali6991550@gmail.com>
2026-03-07 22:05:11 +0500

    30 queryby if want .nottobeindocument()

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Application } from './Components/application/application';
+import { Skills } from './Components/Skills/Skills';
 function App() {
   return (
     <div className="App">
-     <Application/>
+     {/* <Application/> */}
+     
+     <Skills skills={['HTML', 'CSS', 'JavaScript']} />
     </div>
   );
 }
